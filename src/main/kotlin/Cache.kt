84e7b290fd7d2ef24dec515a7616fae7cd07aae8
Shatun <shatungoo@gmail.com>
2024-07-03 14:34:38 +0400

    add gallery

--- src/main/kotlin/Cache.kt
+++ src/main/kotlin/Cache.kt
@@ -12,25 +12,30 @@ import java.io.File
 import java.nio.file.Paths
 import kotlin.io.path.exists
 
-fun main(){
+fun main() {
     val id = "123"
     val url = "https://api-statements.tnet.ge/uploads/statements/1mcs02f666c7d4f87f6e_thumb.jpg"
     getFile(id, url)
 }
 
-fun getFile(id:String, url: String): File {
-    val dir = getTemporalDirectory(id)
-    val fileName = getFileNameFromUrl(url)
-    val file = Paths.get(dir.absolutePath, fileName).toFile()
-    if (!file.exists()) {
-        return runBlocking {
-            downloadImage(url, file)
+fun getFile(id: String, url: String): File? {
+    try {
+        val dir = getTemporalDirectory(id)
+        val fileName = getFileNameFromUrl(url)
+        val file = Paths.get(dir.absolutePath, fileName).toFile()
+        if (!file.exists()) {
+            return runBlocking {
+                downloadImage(url, file)
+            }
         }
+        return file
+    } catch (e: Exception) {
+        println("Error uploading image: $url ${e.message}")
+        return null
     }
-    return file
 }
 
-fun getTemporalDirectory(id:String): File {
+fun getTemporalDirectory(id: String): File {
     val path = Paths.get(System.getProperty("java.io.tmpdir"), appName, id)
     val file = path.toFile()
     file.mkdirs()
--- src/main/kotlin/MainView.kt
+++ src/main/kotlin/MainView.kt
@@ -1,9 +1,12 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.toComposeImageBitmap
@@ -27,36 +30,37 @@ fun MainView(view: MutableState<Views>) {
     }
 }
 
+
 @Composable
 fun FlatCard(flat: Response.Flat) {
     Row {
-//        Image(painterResource("flat.png"), "flat")
         Column {
             Row() {
-                Box(modifier = Modifier.size(300.dp)) {
-                    flat.images[0].thumb?.let {
-                        if (it.contains("static")) return@let
-                        val file = getFile(flat.id.toString(), it)
-                        val image = runBlocking {
-                            file.readBytes().toImageBitmap()
-                        }
-                        println("image: ${file.absolutePath}")
-                        val painter = BitmapPainter(image)
-                        Image(painter, contentDescription = file.absolutePath)
-                    }
-                }
+                val selectedImage = mutableStateOf(0)
+                ImageGallery(flat.id.toString(), flat.images, selectedImage)
                 Column {
-                    Text("id: " + flat.id.toString())
-                    Text("price: " + flat.price["2"]?.price_total.toString())
-                    Text("price per square: " + flat.price["2"]?.price_square.toString())
-                    Text("street id: " + flat.street_id.toString())
-                    Text("floor/total: ${flat.floor.toString()}/${flat.total_floors.toString()}")
-                    Text("room: " + flat.room.toString())
-                    Text("Coordinates: ${flat.lat.toString()}, ${flat.lng.toString()}")
-                    Text("area: " + flat.area.toString())
-                    Text("address: " + flat.address.toString())
-                    Text("city name: " + flat.city_name.toString())
-                    Text("district name: " + flat.district_name.toString())
+                    Text(flat.dynamic_title.toString())
+
+                    Row {
+                        Column {
+                            Text("city name: " + flat.city_name.toString())
+                            Text("urban name: " + flat.urban_name.toString())
+                            Text("district name: " + flat.district_name.toString())
+                            Text("address: " + flat.address.toString())
+                            Text("street: " + flat.street_id.toString())
+                            Text("price: " + flat.price["2"]?.price_total.toString())
+                            Text("price per square: " + flat.price["2"]?.price_square.toString())
+                            Text("floor/total: ${flat.floor.toString()}/${flat.total_floors.toString()}")
+                            Text("room: " + flat.room.toString())
+                            Text("Coordinates: ${flat.lat.toString()}, ${flat.lng.toString()}")
+                            Text("area: " + flat.area.toString())
+                        }
+                        Column {
+                            Text("Comments: ")
+                            Text(flat.comment.toString())
+                        }
+
+                    }
                 }
             }
         }
@@ -64,6 +68,45 @@ fun FlatCard(flat: Response.Flat) {
 
 }
 
+@Composable
+fun ImageGallery(id: String, images: List<Response.Image>, selectedImage: MutableState<Int>) {
+    Box(modifier = Modifier.size(300.dp)) {
+        Row {
+            Button(
+                modifier = Modifier.width(30.dp).fillMaxHeight(),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                enabled = selectedImage.value > 0,
+                onClick = {
+                    selectedImage.value--
+                    println(selectedImage.value)
+                }) { Text("<") }
+            Spacer(modifier = Modifier.weight(1f))
+            Box(modifier = Modifier.fillMaxHeight()) {
+                getFile(id, images[selectedImage.value].thumb!!)?.let { file ->
+                    val image = runBlocking {
+                        file.readBytes().toImageBitmap()
+                    }
+                    println("f${selectedImage.value}: ${file.absolutePath}")
+                    image.let {
+                        val painter = BitmapPainter(it)
+                        Image(painter, contentDescription = file.absolutePath)
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                modifier = Modifier.width(30.dp).fillMaxHeight(),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                enabled = selectedImage.value < images.size - 1,
+                onClick = {
+                    println(selectedImage.value)
+                    selectedImage.value += 1
+                    println(selectedImage.value)
+                }) { Text(">") }
+        }
+
+    }
+}
 
 fun ByteArray.toImageBitmap(): ImageBitmap =
     Image.makeFromEncoded(this).toComposeImageBitmap()
