3d9c7618d78bf4807d2fc5167b45a70598300629
Shatun <shatungoo@gmail.com>
2024-07-25 12:34:56 +0400

    add SnapshotListSerializer

--- src/main/kotlin/com/helldasy/Settings.kt
+++ src/main/kotlin/com/helldasy/Settings.kt
@@ -1,9 +1,12 @@
 package com.helldasy
 
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.encoding.Decoder
@@ -46,11 +49,27 @@ fun loadSettings(): Settings {
 }
 
 fun Settings.saveSettings() {
-    settingsFile().writeText(Json.encodeToString(this))
+    settingsFile().writeText(Json { prettyPrint = true }.encodeToString(this))
 }
 
 class MutableStateSerializer<T>(private val dataSerializer: KSerializer<T>) : KSerializer<MutableState<T>> {
     override val descriptor: SerialDescriptor = dataSerializer.descriptor
     override fun serialize(encoder: Encoder, value: MutableState<T>) = dataSerializer.serialize(encoder, value.value)
     override fun deserialize(decoder: Decoder) = mutableStateOf(dataSerializer.deserialize(decoder))
+}
+
+class SnapshotListSerializer<T>(private val dataSerializer: KSerializer<T>) :
+    KSerializer<SnapshotStateList<T>> {
+
+    override val descriptor: SerialDescriptor = ListSerializer(dataSerializer).descriptor
+    override fun serialize(encoder: Encoder, value: SnapshotStateList<T>) {
+        encoder.encodeSerializableValue(ListSerializer(dataSerializer), value as List<T>)
+    }
+
+    override fun deserialize(decoder: Decoder): SnapshotStateList<T> {
+        val list = mutableStateListOf<T>()
+        val items = decoder.decodeSerializableValue(ListSerializer(dataSerializer))
+        list.addAll(items)
+        return list
+    }
 }
